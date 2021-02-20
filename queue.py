4a0d124068a777ee34dd23a1bbc95fcf0d31e526
CbIpok <dmitrienkomy@gmail.com>
2021-02-20 15:36:06 +0700

    Endless server

---
+++ queue.py
@@ -0,0 +1,10 @@
+
+
+def push (l,el):
+    l.append(el)
+
+def pop(l):
+    el = l[0]
+    del l[0]
+    return el
+
--- server.py
+++ server.py
@@ -1,16 +1,20 @@
 import socket
+import queue
 
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(1)
-conn, addr = sock.accept()
-
-print('connected:', addr)
+q = []
 
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
+    conn, addr = sock.accept()
+    print('connected:', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            conn.close()
+            break
+        queue.push(q, data)
+        print(q)
+        conn.send("Done".encode())
 
-conn.close()
